914fdd9781ada141e243a5400014e614380418ac
Mark Beukers <mark@liquid-state.com>
2019-03-01 15:55:47 +1000

    Apply disabled class to buttons on registration process
    When the form is not yet fully validated. This class will make the button appeared disabled but
    it is still technically functional.

--- generators/entry-iwa/templates/src/pages/Verification.jsx
+++ generators/entry-iwa/templates/src/pages/Verification.jsx
@@ -24,50 +24,59 @@ const Verification = ({
   form: {
     validateFieldsAndScroll,
     getFieldDecorator,
+    getFieldError,
+    getFieldValue,
   },
   onSubmit,
   loading,
-}) => (
-  <Container
-    fixed
-    noPadding
-    className="onboarding"
-  >
-    <img src={register} alt="" />
-    <ContentPadding>
-      <h1>Verification</h1>
-      <p>
+}) => {
+  const disableSubmit = typeof getFieldError('code') !== 'undefined' || !getFieldValue('code');
+  return (
+    <Container fixed noPadding className="onboarding">
+      <img src={register} alt="" />
+      <ContentPadding>
+        <h1>Verification</h1>
+        <p>
           We&apos;ve sent a verification code to the email you entered in the previous step.
           Please check your email for the code and enter it here once you have it.
-      </p>
-      <Form onSubmit={handleSubmit(validateFieldsAndScroll, onSubmit)}>
-        <Form.Item label="Verification code">
-          {getFieldDecorator('code', {
-            rules: [
-              {
-                required: true,
-                message: 'Your verification code is required to continue',
-              },
-              {
-                len: 6,
-                message: 'Your verification code should be 6 digits in length.',
-              },
-            ],
-          })(
-            <Input
-              placeholder="Enter your verification code"
-              pattern="\d*"
-              inputMode="numeric"
-            />,
-          )}
-        </Form.Item>
-        <PinnedToBottom>
-          <Button stretched type="primary" htmlType="submit" loading={loading}>Register</Button>
-        </PinnedToBottom>
-      </Form>
-    </ContentPadding>
-  </Container>
-);
+        </p>
+        <Form onSubmit={handleSubmit(validateFieldsAndScroll, onSubmit)}>
+          <Form.Item label="Verification code">
+            {getFieldDecorator('code', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Your verification code is required to continue',
+                },
+                {
+                  len: 6,
+                  message: 'Your verification code should be 6 digits in length.',
+                },
+              ],
+              })(
+              <Input
+                placeholder="Enter your verification code"
+                pattern="\d*"
+                inputMode="numeric"
+                />,
+            )}
+          </Form.Item>
+          <PinnedToBottom>
+            <Button
+              className={disableSubmit ? 'disabled' : ''}
+              stretched
+              type="primary"
+              htmlType="submit"
+              loading={loading}
+            >
+              Register
+            </Button>
+          </PinnedToBottom>
+        </Form>
+      </ContentPadding>
+    </Container>
+  );
+}
 
 Verification.propTypes = {
   form: PropTypes.object.isRequired,
