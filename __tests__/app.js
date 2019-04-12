cb1a5f257d843922cbc341b390e18d0e1f8fa09e
Mark Beukers <mark@afrotoss.com>
2019-04-12 14:17:23 +1000

    Bugfix/audit (#11)
    * Update packages to fix audit issues
    
    Still one outstanding warning with yeoman-test which just requires them to update sinon, so low impact
    
    * Add longer timeout to jest

--- __tests__/app.js
+++ __tests__/app.js
@@ -2,17 +2,19 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-ls-iwa:app', () => {
-  describe('Default setup', () => {
-    beforeAll(() =>
-      helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
-        yarn: true,
-        name: 'test-project',
-        description: 'test',
-        authorName: 'Chris',
-        authorEmail: 'test@example.com'
-      }));
+jest.setTimeout(20000);
 
+describe('generator-ls-iwa:app', () => {
+  beforeAll(() =>
+    helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      yarn: true,
+      name: 'test-project',
+      description: 'test',
+      authorName: 'Chris',
+      authorEmail: 'test@example.com'
+    }));
+
+  describe('Default setup', () => {
     it('creates package.json', () => {
       assert.file(['package.json']);
     });
