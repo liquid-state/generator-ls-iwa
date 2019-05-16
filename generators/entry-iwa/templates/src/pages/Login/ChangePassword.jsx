f7929418d3a9772096b1d7124a203826afc237f2
Cyril Doussin <cyril@liquid-state.com>
2019-05-16 12:44:34 +1000

    Fix typo and reformat

--- generators/entry-iwa/templates/src/pages/Login/ChangePassword.jsx
+++ generators/entry-iwa/templates/src/pages/Login/ChangePassword.jsx
@@ -5,24 +5,26 @@ import { Container } from '@project/common';
 import { loginChangePasswordSubmitted } from '../../redux/actions/authentication';
 import Form from './ChangePasswordForm';
 
-
 export const ChangePassword = ({ onSubmit }) => (
   <Container fixed className="onboarding">
     <h1>Sign in</h1>
     <p>
-      You must change your password before continuing,
-      change you password below to finish logging in.
+      You must change your password before continuing, change your password below to
+      finish logging in.
     </p>
     <Form onSubmit={onSubmit} />
   </Container>
 );
 
 ChangePassword.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
 };
 
 const actions = {
-  onSubmit: loginChangePasswordSubmitted,
+  onSubmit: loginChangePasswordSubmitted
 };
 
-export default connect(null, actions)(ChangePassword);
+export default connect(
+  null,
+  actions
+)(ChangePassword);
