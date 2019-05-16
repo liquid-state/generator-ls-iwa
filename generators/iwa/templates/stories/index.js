6d1eb01f111209d4d9d67bae1b7b8cd9636eaf23
Cyril Doussin <cyril@liquid-state.com>
2019-05-16 12:45:09 +1000

    Fix import of storybook actions and reformat

--- generators/iwa/templates/stories/index.js
+++ generators/iwa/templates/stories/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { storiesOf, action } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import Index from '../src/pages';
 
 import '../src/style.less';
 
-storiesOf('<%= title %>/Pages', module)
-  .add('Index', () => <Index />)
+storiesOf('<%= title %>/Pages', module).add('Index', () => <Index />);
